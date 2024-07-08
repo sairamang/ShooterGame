c46b54eec1cd7c514b36c7d910cca840c18a188b
vboxuser <vboxuser@UbuntuVM.myguest.virtualbox.org>
2024-07-08 10:14:04 +0530

    First Commit - Build

---
+++ build/CMakeFiles/ShooterGame.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ShooterGame.dir/main.cpp.o"
+  "CMakeFiles/ShooterGame.dir/main.cpp.o.d"
+  "ShooterGame"
+  "ShooterGame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ShooterGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+
+int main()
+{
+    std::cout<<"Simple Shooter Game for Fun"<<std::endl;
+    return 0;
+}
